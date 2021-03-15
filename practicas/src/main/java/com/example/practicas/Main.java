eb713adb544be12d97932057f0ef9aa0b8669d57
mariocgo <e17081448@itmerida.edu.mx>
2021-03-14 19:38:51 -0600

    Mi primer commmit

---
+++ practicas/src/main/java/com/example/practicas/Main.java
@@ -0,0 +1,11 @@
+package com.example.practicas;
+
+public class Main {
+    public static void main(String[] args) {
+        String Hola = "siu";
+        System.out.println(Hola);
+
+        String[] nombres = new String[] {"OVEEWATCH","ELBICHO","P5S","P5R","P5"};
+        System.out.println(nombres[0]);
+    }
+}
